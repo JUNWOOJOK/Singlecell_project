8f4c7038563995f38240dcb25e0b86d26ba29bfe
JunwooJo <78505823+JUNWOOJOK@users.noreply.github.com>
2022-07-21 19:58:56 +0900

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -2,3 +2,5 @@
 for i in $(ls|grep fastq|grep R1);do python ~/final_singlecell_process_for_all.py $(echo $i|cut -d "_" -f 1-4).sam $i ${i}_all_barcode;done
 
 for i in $(ls|grep barcode);do python ~/umicout.py $i > $(echo $i|sed s/.fastq.gz_all_barcode//g).sam5;done
+
+python combine_umi_count.py  > colon19_final
